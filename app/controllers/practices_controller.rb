860aa42b70e835ff5cc7455238b2b42f67bb1e91
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-16 20:34:13 +0900

    コントローラーとビューの記述

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,8 +1,26 @@
 class PracticesController < ApplicationController
+
   def index
     @practices = Practice.order("created_at DESC")
   end
 
   def new
+    @practice = Practice.new
   end
+
+  def create
+    @practice = Practice.new(practice_params)
+    if @practice.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def practice_params
+    params.require(:practice).permit(:title, :text, :lure_id)
+  end
+
 end
