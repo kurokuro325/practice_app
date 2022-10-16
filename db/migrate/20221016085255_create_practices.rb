58e883114e44ed3861dbde73834b14662b3a70ae
kurokuro325 <katapa.zari.123@gmail.com>
2022-10-16 18:20:21 +0900

    マイグレーションファイルの編集

--- db/migrate/20221016085255_create_practices.rb
+++ db/migrate/20221016085255_create_practices.rb
@@ -1,7 +1,9 @@
 class CreatePractices < ActiveRecord::Migration[6.0]
   def change
     create_table :practices do |t|
-
+      t.string     :title        , null: false
+      t.text       :text         , null: false
+      t.integer    :lure_id     , null: false
       t.timestamps
     end
   end
